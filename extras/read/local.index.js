096b1607fe01d621b45d2fb6569a31193eddcebc
Joe Drago <joedrago@gmail.com>
2015-10-18 22:47:46 -0700

    Moved read actions into a dropdown

--- extras/read/local.index.js
+++ extras/read/local.index.js
@@ -1,3 +1,6 @@
+// jquery-dropdown
+jQuery&&function($){function t(t,e){var n=t?$(this):e,d=$(n.attr("data-jq-dropdown")),a=n.hasClass("jq-dropdown-open");if(t){if($(t.target).hasClass("jq-dropdown-ignore"))return;t.preventDefault(),t.stopPropagation()}else if(n!==e.target&&$(e.target).hasClass("jq-dropdown-ignore"))return;o(),a||n.hasClass("jq-dropdown-disabled")||(n.addClass("jq-dropdown-open"),d.data("jq-dropdown-trigger",n).show(),r(),d.trigger("show",{jqDropdown:d,trigger:n}))}function o(t){var o=t?$(t.target).parents().addBack():null;if(o&&o.is(".jq-dropdown")){if(!o.is(".jq-dropdown-menu"))return;if(!o.is("A"))return}$(document).find(".jq-dropdown:visible").each(function(){var t=$(this);t.hide().removeData("jq-dropdown-trigger").trigger("hide",{jqDropdown:t})}),$(document).find(".jq-dropdown-open").removeClass("jq-dropdown-open")}function r(){var t=$(".jq-dropdown:visible").eq(0),o=t.data("jq-dropdown-trigger"),r=o?parseInt(o.attr("data-horizontal-offset")||0,10):null,e=o?parseInt(o.attr("data-vertical-offset")||0,10):null;0!==t.length&&o&&t.css(t.hasClass("jq-dropdown-relative")?{left:t.hasClass("jq-dropdown-anchor-right")?o.position().left-(t.outerWidth(!0)-o.outerWidth(!0))-parseInt(o.css("margin-right"),10)+r:o.position().left+parseInt(o.css("margin-left"),10)+r,top:o.position().top+o.outerHeight(!0)-parseInt(o.css("margin-top"),10)+e}:{left:t.hasClass("jq-dropdown-anchor-right")?o.offset().left-(t.outerWidth()-o.outerWidth())+r:o.offset().left+r,top:o.offset().top+o.outerHeight()+e})}$.extend($.fn,{jqDropdown:function(r,e){switch(r){case"show":return t(null,$(this)),$(this);case"hide":return o(),$(this);case"attach":return $(this).attr("data-jq-dropdown",e);case"detach":return o(),$(this).removeAttr("data-jq-dropdown");case"disable":return $(this).addClass("jq-dropdown-disabled");case"enable":return o(),$(this).removeClass("jq-dropdown-disabled")}}}),window.jqshow=t,$(document).on("click.jq-dropdown","[data-jq-dropdown]",t),window.jqhide=o,$(document).on("click.jq-dropdown",o),window.jqresize=r,$(window).on("resize",r)}(jQuery);
+
 // Draw the progress bars above each entry.
 $(".entry").each(function() {
     var t = $(this);
@@ -6,6 +9,17 @@ $(".entry").each(function() {
         t.prepend("<div class=\"progressbar\"><div class=\"progressbarinner\"></div></div>");
     }
 });
+
+$('body').append(
+    "<div id=\"contextmenu\" class=\"jq-dropdown jq-dropdown-tip\">"+
+        "<ul class=\"jq-dropdown-menu\">"+
+            "<li><a class=\"menuitem\" onclick=\"window.markRead()\">Mark Read</a></li>"+
+            "<li class=\"jq-dropdown-divider\"></li>"+
+            "<li><a class=\"menuitem\" onclick=\"window.markUnread()\">Mark Unread</a></li>"+
+            "<li class=\"jq-dropdown-divider\"></li>"+
+            "<li><a class=\"menuitem\" onclick=\"window.toggleIgnore()\">Toggle Ignore</a></li>"+
+        "</ul>"+
+    "</div>");
 
 function createSection(title, progress) {
     var elem = $("<div class=\"sorted sortowned\"><div class=\"interestsection\">"+title+":</div>");
@@ -62,26 +76,26 @@ window.sorts.push(
   }
 );
 
-window.markignore = function(dir) {
-    window.readRequest({ ignore: dir }, function() {
+window.toggleIgnore = function() {
+    window.readRequest({ ignore: window.dropdownDir }, function() {
         window.resort();
     });
 }
 
-window.markread = function(dir) {
-    if(!confirm("Are you sure you want to mark all of '"+dir+"' as read?")) {
+window.markRead = function() {
+    if(!confirm("Are you sure you want to mark all of '"+window.dropdownDir+"' as read?")) {
         return;
     }
-    window.readRequest({ mark: dir }, function() {
+    window.readRequest({ mark: window.dropdownDir }, function() {
         window.resort();
     });
 }
 
-window.markunread = function(dir) {
-    if(!confirm("Are you sure you want to mark all of '"+dir+"' as unread?")) {
+window.markUnread = function() {
+    if(!confirm("Are you sure you want to mark all of '"+window.dropdownDir+"' as unread?")) {
         return;
     }
-    window.readRequest({ unmark: dir }, function() {
+    window.readRequest({ unmark: window.dropdownDir }, function() {
         window.resort();
     });
 }
@@ -108,25 +122,11 @@ window.readRequest({}, function() {
         var d = t.data('dir');
         var p = t.data('progress');
         if((d != undefined)) {
-            var elem = $("<span class=\"actiontext\">Mark as: </span>");
-            t.append(elem);
-            elem = $("<a class=\"actions\">Read</a>");
+            elem = $("<span data-jq-dropdown=\"#contextmenu\" class=\"menubutton\">[Actions]</span>");
             elem.click(function(event) {
-                window.markread(d);
-            });
-            t.append(elem);
-            elem = $("<span class=\"actiontext\"> / </span>");
-            t.append(elem);
-            elem = $("<a class=\"actions\">Unread</a>");
-            elem.click(function(event) {
-                window.markunread(d);
-            });
-            t.append(elem);
-            elem = $("<span class=\"actiontext\"> / </span>");
-            t.append(elem);
-            elem = $("<a class=\"actions\">Ignore</a>");
-            elem.click(function(event) {
-                window.markignore(d);
+                window.dropdownDir = d;
+                console.log("event", event);
+                window.jqshow.call($(this), event);
             });
             t.append(elem);
         }
